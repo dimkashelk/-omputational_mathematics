5066d94f1cf7f65b88448a104008e020b20a5f9e
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-02-23 13:45:19 +0300

    add calculateDenominator

--- first_lab/Langrage.cpp
+++ first_lab/Langrage.cpp
@@ -1,5 +1,21 @@
 #include "Langrage.h"
 
-dimkashelk::Langrage::Langrage(std::vector<std::pair<double, double>> &points):
-    points_(points)
-{}
+namespace {
+    double calculateDenominator(std::vector<std::pair<double, double> > &points, size_t index) {
+        double res = 1.0;
+        double x = points[index].first;
+        for (size_t i = 0; i < points.size(); i++) {
+            if (i != index) {
+                res *= (x - points[i].first);
+            }
+        }
+        return res;
+    }
+
+    std::vector<double> calculateCoefficients(std::vector<std::pair<double, double> > &points, size_t rank) {
+        std::vector<double> coeff(rank);
+    }
+}
+
+dimkashelk::Langrage::Langrage(std::vector<std::pair<double, double> > &points, size_t rank): points_(points) {
+}
--- first_lab/Langrage.h
+++ first_lab/Langrage.h
@@ -8,9 +8,10 @@ namespace dimkashelk {
     class Langrage {
     public:
         Langrage(std::vector<std::pair<double, double>> &points, size_t rank);
-
+        double getValueFunc(double x);
     private:
         std::vector<std::pair<double, double>> points_;
+        std::vector<double> coeff_;
     };
 
 }
