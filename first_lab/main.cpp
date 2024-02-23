ee20dfe45b490468adb6f570728ba5524bc3211c
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-02-23 16:56:03 +0300

    add spline to main

--- first_lab/main.cpp
+++ first_lab/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "Langrage.h"
+#include "Spline.h"
 
 double f(const double x) {
     return 1 / (1 + x);
@@ -13,10 +14,11 @@ int main() {
         function.emplace_back(0.1 * i, f(0.1 * i));
     }
     dimkashelk::Langrage langrage(function);
-    std::cout << "X   | Langrage | F(x)\n";
+    dimkashelk::Spline spline(function);
+    std::cout << "X   | Langrage | spline | F(x)\n";
     for (int i = 0; i < 10; i++) {
         double x = 0.05 + 0.1 * i;
-        std::cout << x << " " << langrage(x) << " " << f(x) << std::endl;
+        std::cout << x << " | " << langrage(x) << " | " << spline(x) << " | " << f(x) << std::endl;
     }
     return 0;
 }
