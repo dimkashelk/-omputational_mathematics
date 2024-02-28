8d7ec9cce4d5a2385574e09a78a7cbd889feb3cf
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-02-28 21:55:39 +0300

    update

--- second_lab/Solve.cpp
+++ second_lab/Solve.cpp
@@ -1,5 +1,9 @@
 #include "Solve.h"
 
+#include <stdexcept>
+
+#include "Decomp.h"
+
 int dimkashelk::details::solve(int n, int ndim,
                                double *a, double b[],
                                int pivot[])
@@ -53,7 +57,32 @@ int dimkashelk::details::solve(int n, int ndim,
     return (0);
 } /* --- end function solve() --- */
 
-dimkashelk::Solve::Solve(std::vector<std::vector<double> > &matrix_left,
-                         std::vector<double> &matrix_right): data_(nullptr),
-                                                             pivot_(nullptr) {
+dimkashelk::Solve::Solve(): data_right_(nullptr) {
+}
+
+void dimkashelk::Solve::operator()(const std::vector<std::vector<double> > &matrix_left,
+                                   std::vector<double> &matrix_right) {
+    if (matrix_left.size() != matrix_right.size()) {
+        throw std::logic_error("Check data");
+    }
+    free();
+
+    data_right_ = new double[matrix_right.size()];
+    for (int i = 0; i < matrix_right.size(); i++) {
+        data_right_[i] = matrix_right[i];
+    }
+    Decomp dec;
+    dec(matrix_left);
+    int size = static_cast<int>(matrix_left.size());
+    details::solve(size, size, dec.data_, data_right_, dec.pivot_);
+}
+
+dimkashelk::Solve::~Solve() {
+    free();
+}
+
+void dimkashelk::Solve::free() const {
+    if (data_right_ != nullptr) {
+        delete[] data_right_;
+    }
 }
--- second_lab/Solve.h
+++ second_lab/Solve.h
@@ -1,22 +1,31 @@
 #ifndef SOLVE_H
 #define SOLVE_H
-#define AINDEX(i,j) (i * ndim + j)
 #include <vector>
+#include "Decomp.h"
 
 namespace dimkashelk {
     namespace details {
         int solve(int n, int ndim,
-              double *a, double b[],
-              int pivot[]);
+                  double *a, double b[],
+                  int pivot[]);
     }
+
+    class Decomp;
+
     class Solve {
+        friend class Decomp;
+
     public:
-        Solve(std::vector<std::vector<double>> &matrix_left, std::vector<double> &matrix_right);
+        Solve();
+
+        void operator()(const std::vector<std::vector<double> > &matrix_left, std::vector<double> &matrix_right);
+
+        ~Solve();
+
     private:
-        int size_left_;
-        int size_right_;
-        double *data_;
-        int *pivot_;
+        double *data_right_;
+
+        void free() const;
     };
 }
 
